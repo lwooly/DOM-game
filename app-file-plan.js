cd0bbad3b0ed8b59c4be11ea32126fa542d7003f
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2023-09-11 18:56:15 +0100

    Create app-file-plan.js

---
+++ app-file-plan.js
@@ -0,0 +1,20 @@
+//Minesweeper grid square classes  - initial thoughts similar to the todo app
+//These could then be rendered based on the properties in the grid square class
+class gridSquare {
+constructor(mine = false, selected = false) {
+  this._id = nanoid()
+  this.mine = false
+  this.selected = false
+}
+
+class gridSquareManager {
+  constructor() {
+  this.todos = create all grid square function
+  }
+//make mine
+
+// select square
+
+}
+
+
