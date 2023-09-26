605fe57f4322419a4c739c5bab0f201165c28fe1
Aidan Meehan <aodhan66@gmail.com>
2023-09-26 17:54:30 +0100

    created functions to randomly generate 3 tile ships that do not overlap, and give the appropriate squares the class of ship

--- aidantest/scripts/aidan.js
+++ aidantest/scripts/aidan.js
@@ -12,243 +12,263 @@
 //   - give it a toggleable class on selection =======================================
 //   - random number generator to pick column, then another to pick row for starting ship tile ======================================
 //     - another random gen to pick horizontal or vertical ========================================
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
 //   TODO abstract this out into a single function to generate one ship made of 3-4 pieces
 
-// VARIABLES
-const lowBound = 1
-const highBound = 10
-const limitBound = 8
+//* --------- //
+//* VARIABLES //
+//* --------- //
+// Bounding values for selecting ship squares
+//   The limit bound is used to stop ships from going off the grid tileset
+const lowBound = 1;
+const highBound = 10;
+const limitBound = 8;
 
-// let direction = getRndInteger(0,1)
-// console.log(direction)
-let xAxis = getRndInteger(lowBound, highBound)
-// console.log(xAxis)
-let yAxis = getRndInteger(lowBound, highBound)
-// console.log(yAxis)
-let row = (xAxis + 9).toString(36)
-// console.log(row)
-let ship1class = row.concat('.', yAxis)
-// console.log(ship1class)
+//* --------- //
+//* Functions //
+//* --------- //
+//* Function to generate a random integer between an upper and lower bound, inclusive of lower and upper bound [returns number]
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-const ship1 = document.querySelectorAll('.' + row)[yAxis-1]
-console.log(ship1)
-// ship1.classList.add('ship')
-
-
-
-// Functions
-// Generate direction for the ship [returns 'horizontal' or 'vertical' string]
+//* Function to generate a random direction for the ship [returns 'horizontal' or 'vertical' string]
 function direction() {
-    const dir = getRndInteger(0,1)
-    if (dir) {
-        return 'vertical'
-    } else {
-        return 'horizontal'
-    }
+  const dir = getRndInteger(0, 1);
+  if (dir) {
+    return "vertical";
+  } else {
+    return "horizontal";
+  }
 }
 // console.log(direction())
 
-// Generate X and Y axis for a single point [returns number and letter for the point]
+//* Function to generate Y axis for a single point [returns object of 3 numbers for the ship]
 function getNumberAxis(low, high) {
-    return getRndInteger(low-1, high-1)
+  const num = getRndInteger(low - 1, high - 1);
+  const num2 = num + 1;
+  const num3 = num + 2;
+  // DEBUG
+  // console.log('numberaxisobject', {
+  //     num: String(num),
+  //     num2: String(num2),
+  //     num3: String(num3)
+  // })
+  return {
+    num: String(num),
+    num2: String(num2),
+    num3: String(num3),
+  };
 }
 // console.log('get y axis number function', getNumberAxis(lowBound, highBound))
 
+//* Function to generate X axis for a single point [returns object of 3 letters for the ship]
 function getLetterAxis(low, high) {
-    const num = getRndInteger(low, high);
-    const letter = (num + 9).toString(36).toUpperCase()
-    const letter2 = (num + 10).toString(36).toUpperCase()
-    const letter3 = (num + 11).toString(36).toUpperCase()
-    return {
-        letter: letter,
-        letter2: letter2,
-        letter3: letter3
-    }
-    // return letter.toUpperCase()
-    // DEBUG
-    // Returns number and letter for X axis, make sure they are correct
-    // console.log(num + letter + letter2 + letter3)
-    // return num + letter
+  const num = getRndInteger(low, high);
+  const letter = (num + 9).toString(36).toUpperCase();
+  const letter2 = (num + 10).toString(36).toUpperCase();
+  const letter3 = (num + 11).toString(36).toUpperCase();
+  // DEBUG
+  // Logs number and letter for X axis, make sure they are correct
+  // console.log(num + letter + letter2 + letter3)
+  return {
+    letter: letter,
+    letter2: letter2,
+    letter3: letter3,
+  };
 }
 // console.log('get x axis letter function', getLetterAxis(lowBound, highBound))
 
-// console.log(1, String(1), '1')
-
-
-// Test vertical ship
-// get tile 1 coordinates
-const tile1Y = getNumberAxis(lowBound, limitBound)
-const tile1X = getLetterAxis(lowBound, highBound)
-// console.log('get letter axis object', tile1X)
-const tile1 = String(tile1Y + tile1X.letter)
-// with the class selector period
-const tile1period = String('.' + tile1Y + tile1X.letter)
-console.log('tile 1 class code', tile1)
-// get tile 2 coordinates
-const tile2Y = tile1Y + 1
-const tile2 = String(tile2Y + tile1X.letter)
-console.log('tile 2 class code', tile2)
-// get tile 3 coordinates
-const tile3Y = tile1Y + 2
-const tile3 = String(tile3Y + tile1X.letter)
-console.log('tile 2 class code', tile3)
-console.log({
-    tile1: tile1,
-    tile2: tile2,
-    tile3: tile3
-})
-
-// console.log('getelementbyclassname', document.getElementsByClassName('1A'))
-console.log('getelementbyclassname', document.getElementsByClassName(tile1))
-const ship11 = document.getElementsByClassName(tile1)
-console.log(ship11[0])
-const ship12 = document.getElementsByClassName(tile2)
-console.log(ship12[0])
-const ship13 = document.getElementsByClassName(tile3)
-console.log(ship13[0])
-ship11[0].classList.add('ship')
-ship12[0].classList.add('ship')
-ship13[0].classList.add('ship')
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Function to randomly generate a vertical ship [returns an object with the 3 tile codes as strings]
+//* Function to randomly generate a vertical ship [returns an object with the 3 tile codes as strings]
 function genVertShip() {
-// get tile 1 coordinates
-const tile1Y = getNumberAxis(lowBound, limitBound)
-const tile1X = getLetterAxis(lowBound, highBound)
-// console.log('get letter axis object', tile1X)
-const tile1 = String(tile1Y + tile1X.letter)
-// with the class selector period
-// console.log('tile 1 class code', tile1)
-// get tile 2 coordinates
-const tile2Y = tile1Y + 1
-const tile2 = String(tile2Y + tile1X.letter)
-// console.log('tile 2 class code', tile2)
-// get tile 3 coordinates
-const tile3Y = tile1Y + 2
-const tile3 = String(tile3Y + tile1X.letter)
-// console.log('tile 2 class code', tile3)
-// console.log('vertship class codes', tile1, tile2, tile3)
+  // generate tiles
+  const tileNumbers = getNumberAxis(lowBound, limitBound);
+  const tileLetters = getLetterAxis(lowBound, highBound);
+  // get tile 1 coordinates
+  const tile1 = String(tileNumbers.num + tileLetters.letter);
+  // get tile 2 coordinates
+  const tile2 = String(tileNumbers.num2 + tileLetters.letter);
+  // get tile 3 coordinates
+  const tile3 = String(tileNumbers.num3 + tileLetters.letter);
+  // DEBUG
+  // console.log('get letter axis object', tile1X)
+  // console.log('tile 1 class code', tile1)
+  // console.log('tile 2 class code', tile2)
+  // console.log('tile 2 class code', tile3)
+  // console.log('vertship class codes', tile1, tile2, tile3)
 
-// returns vertical ship object with classes inside
-return {
+  // returns vertical ship object with classes inside
+  return {
     tile1: tile1,
     tile2: tile2,
-    tile3: tile3
-}}
-console.log('genvertship', genVertShip())
+    tile3: tile3,
+  };
+}
+// console.log('genvertship', genVertShip())
 
-// Function to randomly generate a horizontal ship [returns an object with the 3 tile codes as strings]
+//* Function to randomly generate a horizontal ship [returns an object with the 3 tile codes as strings]
 function genHorizShip() {
-    // get tile 1 coordinates
-    const tile1Y = getNumberAxis(lowBound, highBound)
-    const tile1X = getLetterAxis(lowBound, limitBound)
-    console.log('get letter axis object', tile1X)
-    const tile1 = String(tile1Y + tile1X.letter)
-    // with the class selector period
-    // console.log('tile 1 class code', tile1)
-    // get tile 2 coordinates
-    const tile2 = String(tile1Y + tile1X.letter2)
-    // console.log('tile 2 class code', tile2)
-    // get tile 3 coordinates
-    const tile3 = String(tile1Y + tile1X.letter3)
-    // console.log('tile 2 class code', tile3)
-    console.log('horizship class codes', tile1, tile2, tile3)
-    
-// returns vertical ship object with classes inside
-return {
+  // generate tiles
+  const tileNumbers = getNumberAxis(lowBound, highBound);
+  const tileLetters = getLetterAxis(lowBound, limitBound);
+  // get tile 1 coordinates
+  const tile1 = String(tileNumbers.num + tileLetters.letter);
+  // get tile 2 coordinates
+  const tile2 = String(tileNumbers.num + tileLetters.letter2);
+  // get tile 3 coordinates
+  const tile3 = String(tileNumbers.num + tileLetters.letter3);
+  // DEBUG
+  // console.log('get letter axis object', tile1X)
+  // console.log('tile 1 class code', tile1)
+  // console.log('tile 2 class code', tile2)
+  // console.log('tile 2 class code', tile3)
+  // console.log('horizship class codes', tile1, tile2, tile3)
+
+  // returns horizontal ship object with classes inside
+  return {
     tile1: tile1,
     tile2: tile2,
-    tile3: tile3
-}}
-console.log('genhoirzship', genHorizShip())
+    tile3: tile3,
+  };
+}
+// console.log('genhoirzship', genHorizShip())
 
-// Function to apply the class of '.ship' to the elements returned from genVertShip and genHorizShip, it will then be placed with them inside genShip
-function shipClassifier() {
-    //! TODO this is your initial code to work with ===============================================================================================
-    // console.log('getelementbyclassname', document.getElementsByClassName('1A'))
-console.log('getelementbyclassname', document.getElementsByClassName(tile1))
-const ship11 = document.getElementsByClassName(tile1)
-console.log(ship11[0])
-const ship12 = document.getElementsByClassName(tile2)
-console.log(ship12[0])
-const ship13 = document.getElementsByClassName(tile3)
-console.log(ship13[0])
-ship11[0].classList.add('ship')
-ship12[0].classList.add('ship')
-ship13[0].classList.add('ship')
+//* Function to apply the class of '.ship' to the elements returned by genVertShip and genHorizShip, it will then be placed with them inside genShip
+function shipClassifier(ship) {
+  // grabs the tile codes of the ship squares
+  const tile1 = String(ship.tile1);
+  const tile2 = String(ship.tile2);
+  const tile3 = String(ship.tile3);
+  //   grabs the elements from the document
+  const shipTile1 = document.getElementsByClassName(tile1);
+  const shipTile2 = document.getElementsByClassName(tile2);
+  const shipTile3 = document.getElementsByClassName(tile3);
+  // adds the ship class to the elements
+  shipTile1[0].classList.add("ship");
+  shipTile2[0].classList.add("ship");
+  shipTile3[0].classList.add("ship");
+  // DEBUG
+  // console.log('getelementbyclassname', document.getElementsByClassName(tile1))
+  // console.log(shipTile1[0])
+  // console.log(shipTile2[0])
+  // console.log(shipTile3[0])
 }
 
+//* Function that checks if 3 ship tiles returned by genVertShip and genHorizShip are already picked as a ship [returns a boolean, ship = true, not ship = false]
+function shipChecker(ship) {
+  // grabs the tile codes of the ship squares
+  const tile1 = String(ship.tile1);
+  const tile2 = String(ship.tile2);
+  const tile3 = String(ship.tile3);
+  //   grabs the elements from the document
+  const shipTile1 = document.getElementsByClassName(tile1);
+  const shipTile2 = document.getElementsByClassName(tile2);
+  const shipTile3 = document.getElementsByClassName(tile3);
+  // DEBUG
+  // console.log('getelementbyclassname', document.getElementsByClassName(tile1))
+  // console.log(shipTile1[0])
+  // console.log(shipTile2[0])
+  // console.log(shipTile3[0])
 
+  // checks if the ship class is on the elements
+  if (
+    shipTile1[0].classList.contains("ship") ||
+    shipTile2[0].classList.contains("ship") ||
+    shipTile3[0].classList.contains("ship")
+  ) {
+    // console.log('This is already a ship')
+    return true;
+  } else {
+    // console.log('This is a new valid ship')
+    return false;
+  }
+}
 
-
-// Function that randomly picks horizontal or vertical, then generates 3 ship tiles in sequence [returns an object with the 3 tile codes as strings]
+//* Function that randomly picks horizontal or vertical, and generates a random ship on that axis [returns the generated ship object with the 3 tile codes as strings]
 function genShip() {
-    // Gets random direction for the ship
-    let dir = direction()
-    console.log(dir)
-    // We make every ship go in a positive direction (ships go left to right and top to bottom)
-    // if the ship is vertical we limit the upper bound for the first tile, so it can only start from the 7th row, and not lower, but it can be any column
-    if(dir === 'vertical') {
-    } else {
-    // if the ship is horizontal, we limit the upper bound again, so it can only start from the 7th column (G) and not further to the right, but it can be any row
-    }
+  // Gets random direction for the ship
+  let dir = direction();
+  // DEBUG
+  // Check the direction of the ship is correct
+  // console.log(dir);
+  // generates a ship based on the direction
+  if (dir === "vertical") {
+    return genVertShip();
+  } else {
+    return genHorizShip();
+  }
 }
 
-// Function that 
+//* Function that incorporates the others
+//*     Generates a ship
+//*     Checks if it's already a ship
+//*     If it's already a ship, generates another ship and repeats
+//*     If it's not a ship, classifies those tiles as ship tiles
+function genValidShip() {
+  // Create a new ship
+  let newShip = genShip();
+  // Check that the ship is valid
+  do {
+    newShip = genShip();
+  } while (shipChecker(newShip));
+  // insert the valid ship into the page
+  shipClassifier(newShip);
+//   console.log("valid ship generated");
+}
 
+//* ------------ //
+//* TEST SECTION //
+//* ------------ //
+//* Testing Calls
+const testVertShip = genVertShip();
+const testHorizShip = genHorizShip();
+const testShip1 = genShip();
+const testShip2 = genShip();
+// console.log("Test Vertical Ship", testVertShip);
+// console.log("Test Horizontal Ship", testHorizShip);
+console.log("Test Random Ship 1", testShip1);
+console.log("Test Random Ship 2", testShip2);
+// console.log('test random ship', genShip())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// shipClassifier(testHorizShip);
+// shipClassifier(testVertShip);
+// shipClassifier(testShip1);
+// shipClassifier(testShip2);
+// shipChecker(testHorizShip)
+// console.log(shipChecker(testVertShip))
+genValidShip();
+genValidShip();
+genValidShip();
+genValidShip();
+genValidShip();
+genValidShip();
+genValidShip();
+genValidShip();
+genValidShip();
+genValidShip();
+genValidShip();
 
 // - have the tiles be clickable =====================================
 //   - check how event listeners work with regards to clicking specific tiles ==============================
 //   - give it a toggleable class on click =========================================
 
 //selectors
-const board = document.querySelector('.board')
+const board = document.querySelector(".board");
 
 //bindings
-board.addEventListener('click', (event) => {
-    console.log(event)
-    const selectedTile = event.target
-    console.log(selectedTile.classList)
-    selectedTile.classList.toggle('shot')
-    console.log(selectedTile.classList)
+board.addEventListener("click", (event) => {
+  // console.log(event)
+  const selectedTile = event.target;
+  // console.log(selectedTile.classList)
+  selectedTile.classList.add("shot");
+  // console.log(selectedTile.classList)
 
-    if(selectedTile.classList.contains('ship')) {
-        console.log('hit')
-    }
-})
-
+  if (selectedTile.classList.contains("ship")) {
+    selectedTile.classList.add("destroyed");
+    console.log("hit");
+  } else {
+    console.log("miss");
+  }
+});
 
 // - detect if a ship is below on click ==========================
 //   - then render ship
