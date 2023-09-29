c61b0a6a99f9496aedb69666f47234bbf4c221cf
Aidan Meehan <aodhan66@gmail.com>
2023-09-29 18:32:18 +0100

    made end game screens readable

--- workingCopyDragBug/scripts/gameOver.js
+++ workingCopyDragBug/scripts/gameOver.js
@@ -1,11 +1,11 @@
 export function gameOver (turnManager, boardNode) {
     const message = document.createElement('div')
-    message.className = 'game-over'
+    message.className = 'game-over bg-dark text-info'
 
     if (turnManager.player()) {
-        message.innerHTML = `<h2>Game Over</h2> <p>Computer has won! </p>`
+        message.innerHTML = `<h1>Game Over</h1> <p>Computer has won!</p>`
     } else {
-        message.innerHTML = `<h2>Game Over</h2> <p>Player has won! </p>`
+        message.innerHTML = `<h1>Game Over</h1> <p>Player has won!</p>`
     }
     
     boardNode.replaceChildren(message)
