d575f2870b11ebb1790191e675d7449f18d1b323
Aidan Meehan <aodhan66@gmail.com>
2023-09-29 18:03:22 +0100

    some CSS changes to the ships container, and removing more uneeded code

--- workingCopyDragBug/scripts/gameOver.js
+++ workingCopyDragBug/scripts/gameOver.js
@@ -3,9 +3,9 @@ export function gameOver (turnManager, boardNode) {
     message.className = 'game-over'
 
     if (turnManager.player()) {
-        message.innerHTML = `<h2 class="text-info">Game Over</h2> <p class="text-info">Computer has won! </p>`
+        message.innerHTML = `<h2>Game Over</h2> <p>Computer has won! </p>`
     } else {
-        message.innerHTML = `<h2 class="text-info">Game Over</h2> <p class="text-info">Player has won! </p>`
+        message.innerHTML = `<h2>Game Over</h2> <p>Player has won! </p>`
     }
     
     boardNode.replaceChildren(message)
